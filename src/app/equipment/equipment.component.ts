31b023a45c5221afa4f253f3ff97bb9200d39364
Ryan Doherty <rjd.atx@gmail.com>
2019-12-15 17:27:17 -0600

    30.7 Studio: Angular, Part 3 -- Part 1. Equipment

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -27,5 +27,18 @@ export class EquipmentComponent implements OnInit {
    ngOnInit() { }
 
    // Code your addItem function here:
+   addItem(equipment: object): boolean
+   {
+      this.cargoHold.push(equipment);
+      this.cargoMass += equipment['mass'];
+      if(this.maximumAllowedMass - this.cargoMass < 200)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+   }
    
 }
