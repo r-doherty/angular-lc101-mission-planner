1b7a29aa00a7f74bc405ef6930c21ec688d04c81
Ryan Doherty <rjd.atx@gmail.com>
2019-12-15 18:19:06 -0600

    30.7 Studio: Angular, Part 3 -- Part 2: Select Crew Members

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -11,13 +11,13 @@ export class CrewComponent implements OnInit {
   crew: object[] = [];
 
   candidates: object[] = [
-    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
-    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
-    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
-    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
-    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
-    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
-    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg', inspect: false},
+    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg', inspect: false},
+    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg', inspect: false},
+    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg', inspect: false},
+    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg', inspect: false},
+    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg', inspect: false},
+    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg', inspect: false}
   ];
 
   constructor() { }
@@ -25,5 +25,26 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+  addCrewMember(candidate: object)
+  {
+    if(this.crew.includes(candidate))
+    {
+      this.inCrew = true;
+    }
+    else
+    {
+      this.inCrew = false;
+    }
+
+    if(this.inCrew)
+    {
+      let index = this.crew.indexOf(candidate);
+      this.crew.splice(index, 1);
+    }
+    else if(!this.inCrew && this.crew.length < 3)
+    {
+      this.crew.push(candidate);
+    }
+  }
 
 }
